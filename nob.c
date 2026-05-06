e3c7da00fb26d10ce9ae3c18d5db18866129a644
rexim <reximkut@gmail.com>
2026-05-06 21:12:32 +0700

    nob: introduce -trace flag

--- nob.c
+++ nob.c
@@ -1,6 +1,8 @@
 #define NOB_IMPLEMENTATION
 #define NOB_STRIP_PREFIX
 #include "thirdparty/nob.h"
+#define FLAG_IMPLEMENTATION
+#include "thirdparty/flag.h"
 
 #define BUILD_FOLDER      "build/"
 #define SRC_FOLDER        "src/"
@@ -12,21 +14,48 @@ static Procs procs = {0};
 
 static void compile(void)
 {
-    cmd_append(&cmd, "cc");
+    cmd_append(&cmd, "clang");
     cmd_append(&cmd, "-Wall");
     cmd_append(&cmd, "-Wextra");
     cmd_append(&cmd, "-Wswitch-enum");
+    cmd_append(&cmd, "-Wno-unused-function");
+    cmd_append(&cmd, "-fsanitize=memory,undefined");
     cmd_append(&cmd, "-I.");
     cmd_append(&cmd, "-I"THIRDPARTY_FOLDER);
+}
+
+void print_usage(void)
+{
+    fprintf(stderr, "Usage: %s [OPTIONS]\n", flag_program_name());
+    fprintf(stderr, "OPTIONS:\n");
+    flag_print_options(stderr);
 }
 
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
+    bool trace = false;
+    bool help = false;
+
+    flag_bool_var(&trace, "trace", false, "Enabling tracing of the location where each single line was generated");
+    flag_bool_var(&help, "help", false, "Print this help message");
+
+    if (!flag_parse(argc, argv)) {
+        print_usage();
+        flag_print_options(stderr);
+        return 1;
+    }
+
+    if (help) {
+        print_usage();
+        return 0;
+    }
+
     if (!mkdir_if_not_exists(BUILD_FOLDER)) return 1;
 
     compile();
+    if (trace) cmd_append(&cmd, "-DGEN_TRACE");
     cmd_append(&cmd, "-ggdb");
     cmd_append(&cmd, "-o", BUILD_FOLDER"lag");
     cmd_append(&cmd, SRC_FOLDER"lag.c");
